34f8aca9c076ab100170e7a6f1237784c7fbee1b
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-12 10:18:49 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/platforms/Youtube.py
+++ Oneforall/platforms/Youtube.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 API_URL = "http://45.77.174.241:9090"
 
 
-class YouTubeAPI:
+class YouTube:
 
     def __init__(self):
         self.api = API_URL
@@ -15,76 +15,112 @@ class YouTubeAPI:
 
     async def search(self, query: str, limit: int = 1):
         try:
-            results = VideosSearch(query, limit=limit)
+            if not query:
+                return []
+
+            results = VideosSearch(query.strip(), limit=limit)
             data = await results.next()
+
+            if not data:
+                return []
+
             return data.get("result", [])
+
         except Exception:
             return []
 
     async def get_video_id(self, query: str):
         results = await self.search(query)
+
         if not results:
             return None
+
         try:
             return results[0]["id"]
         except Exception:
             return None
 
     async def get_audio(self, video_id: str):
+        if not video_id:
+            return None, None
+
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
                     f"{self.api}/download",
                     params={"url": video_id, "type": "audio"}
                 ) as res:
+
                     if res.status != 200:
                         return None, None
+
                     data = await res.json()
                     token = data.get("download_token")
+
                     if not token:
                         return None, None
-                    return f"{self.api}/stream/{video_id}?type=audio", token
+
+                    stream_url = f"{self.api}/stream/{video_id}?type=audio"
+                    return stream_url, token
+
         except Exception:
             return None, None
 
     async def get_video(self, video_id: str):
+        if not video_id:
+            return None, None
+
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
                     f"{self.api}/download",
                     params={"url": video_id, "type": "video"}
                 ) as res:
+
                     if res.status != 200:
                         return None, None
+
                     data = await res.json()
                     token = data.get("download_token")
+
                     if not token:
                         return None, None
-                    return f"{self.api}/stream/{video_id}?type=video", token
+
+                    stream_url = f"{self.api}/stream/{video_id}?type=video"
+                    return stream_url, token
+
         except Exception:
             return None, None
 
     async def url(self, message: Message):
-        messages = [message]
+        try:
+            messages = [message]
 
-        if message.reply_to_message:
-            messages.append(message.reply_to_message)
+            if message.reply_to_message:
+                messages.append(message.reply_to_message)
 
-        for msg in messages:
-            if msg.entities:
-                for entity in msg.entities:
-                    if entity.type == MessageEntityType.URL:
-                        text = msg.text or msg.caption
-                        return text[entity.offset: entity.offset + entity.length]
+            for msg in messages:
 
-            if msg.caption_entities:
-                for entity in msg.caption_entities:
-                    if entity.type == MessageEntityType.TEXT_LINK:
-                        return entity.url
+                if msg.entities:
+                    for entity in msg.entities:
+                        if entity.type == MessageEntityType.URL:
+                            text = msg.text or msg.caption
+                            if text:
+                                return text[entity.offset: entity.offset + entity.length]
 
-        return None
+                if msg.caption_entities:
+                    for entity in msg.caption_entities:
+                        if entity.type == MessageEntityType.TEXT_LINK:
+                            return entity.url
+
+            return None
+
+        except Exception:
+            return None
 
     async def exists(self, link: str, videoid: bool = None):
+        if not link:
+            return False
         return bool(re.search(self.regex, link))
 
     async def title(self, link: str, videoid: bool = None):
@@ -104,15 +140,16 @@ class YouTubeAPI:
 
     async def play_audio(self, query: str):
         video_id = await self.get_video_id(query)
+
         if not video_id:
             return None, None
+
         return await self.get_audio(video_id)
 
     async def play_video(self, query: str):
         video_id = await self.get_video_id(query)
+
         if not video_id:
             return None, None
-        return await self.get_video(video_id)
 
-print("QUERY:", query)
-print("RESULT:", results)
+        return await self.get_video(video_id)
