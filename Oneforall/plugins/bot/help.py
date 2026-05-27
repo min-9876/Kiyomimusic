2e84154bbd3df810400fa3ed5789c0a73103ccee
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-27 13:18:43 +0530

    Update help.py

--- Oneforall/plugins/bot/help.py
+++ Oneforall/plugins/bot/help.py
@@ -42,7 +42,7 @@ async def helper_private(
         keyboard = help_pannel(_)
 
         await update.reply_video(
-            video="https://files.catbox.moe/ix1sik.mp4",
+            video="https://graph.org/file/73ea9acced23446c9b3a4-fcf055f9f3c2dfc824.mp4",
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
         )
