1653f368ce5f6da7676780ae9a02facb1191b92b
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-27 13:52:40 +0530

    Update pfp.py

--- Oneforall/plugins/sudo/pfp.py
+++ Oneforall/plugins/sudo/pfp.py
@@ -1,34 +1,34 @@
-import requests
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import OWNER_ID, BOT_TOKEN
+from config import OWNER_ID
 from Oneforall import app
 
 
 @app.on_message(filters.command("setpfp") & filters.private)
 async def set_pfp(client, message: Message):
 
-    if message.from_user.id != OWNER_ID:
-        return await message.reply_text("not authorized.")
+    user_id = message.from_user.id
 
-    if not message.reply_to_message or not message.reply_to_message.photo:
+    # owner check
+    if user_id != OWNER_ID:
+        return await message.reply_text("you are not allowed to use this command.")
+
+    # check replied photo
+    if not message.reply_to_message:
         return await message.reply_text("reply to a photo.")
 
-    msg = await message.reply_text("updating bot pfp...")
+    if not message.reply_to_message.photo:
+        return await message.reply_text("reply to a valid photo.")
 
     try:
+        # download photo
         photo_path = await message.reply_to_message.download()
 
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/setUserProfilePhotos"
+        # change bot pfp
+        await client.set_profile_photo(photo=photo_path)
 
-        with open(photo_path, "rb") as photo:
-            response = requests.post(
-                url,
-                files={"photo": photo}
-            )
-
-        await msg.edit(response.text)
+        await message.reply_text("bot profile photo updated successfully.")
 
     except Exception as e:
-        await msg.edit(str(e))
+        await message.reply_text(f"error:\n\n{e}")
